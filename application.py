d5490113134ce2176b4aadfc2302b49b2e044724
Praditya Raudi <pradityaraudi@gmail.com>
2021-10-29 18:03:03 -0400

    update application.py and requirements.txt for swagger compatible

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
 from flask import Flask, Response, request
 from flask_cors import CORS
+from flask_restful import Resource, Api
+from apispec import APISpec
+from marshmallow import Schema, fields
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+from flask_apispec.views import MethodResource
+from flask_apispec import marshal_with, doc, use_kwargs
 import json
 import uuid
 
@@ -13,69 +20,194 @@ from application_services.FriendsResource.friends_service import FriendsResource
 from utils import rest_utils
 
 application = Flask(__name__)
+api = Api(application)  # Flask restful wraps Flask app around it.
+application.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Friend Service',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
+
 CORS(application)
+docs = FlaskApiSpec(application)
 
 
-@application.route('/friends/<user>', methods=["GET"])
-def get_friends(user):
-    try:
-        user = str(user)
-        res = FriendsResource.get_friends(user)
-        rsp = Response(json.dumps(res), status=200, content_type="application/json")
-    except Exception as e:
-        # HTTP status code.
-        logging.error("/friends/<user>, e = {}".format(e))
-        rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+class GetFriendResponseSchema(Schema):
+    message = fields.Str(default='Success')
 
-    return rsp
+class PostResponseSchema(Schema):
+    status = fields.Bool(default=True)
+
+class FriendRequestSchema(Schema):
+    friend_id = fields.String(required=True, description="user_id of the friend")
+
+
+#  Restful way of creating APIs through Flask Restful
+class GetFriendAPI(MethodResource, Resource):
+    @doc(description='Get list of all friends of <user_id>.', tags=['GetFriend'])
+    @marshal_with(GetFriendResponseSchema)  # marshalling
+    def get(self, user):
+        '''
+        Get method represents a GET API method
+        '''
+        try:
+            user = str(user)
+            res = FriendsResource.get_friends(user)
+            rsp = Response(json.dumps(res), status=200, content_type="application/json")
+        except Exception as e:
+            # HTTP status code.
+            logging.error("/friends/<user>, e = {}".format(e))
+            rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+
+        return rsp
+
+    # @doc(description='My First GET GetFriend API.', tags=['Awesome'])
+    # @use_kwargs(GetFriendRequestSchema, location=('json'))
+    # @marshal_with(GetFriendResponseSchema)  # marshalling
+    # def post(self, **kwargs):
+    #     '''
+    #     Get method represents a GET API method
+    #     '''
+    #     return {'message': 'My First GetFriend API'}
+
+#  Restful way of creating APIs through Flask Restful
+class GetPendingFriendAPI(MethodResource, Resource):
+    @doc(description='Get list of all pending friends that added <user_id>.', tags=['GetPendingFriend'])
+    @marshal_with(GetFriendResponseSchema)  # marshalling
+    def get(self, user):
+        '''
+        Get method represents a GET API method
+        '''
+        try:
+            user = str(user)
+            res = FriendsResource.get_pending_friends(user)
+            rsp = Response(json.dumps(res), status=200, content_type="application/json")
+        except Exception as e:
+            # HTTP status code.
+            logging.error("/friends/<user>/pending, e = {}".format(e))
+            rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+
+        return rsp
+
+#  Restful way of creating APIs through Flask Restful
+class GetRequestFriendAPI(MethodResource, Resource):
+    @doc(description='Get list of all pending friends <user_id> added.', tags=['GetRequestFriend'])
+    @marshal_with(GetFriendResponseSchema)  # marshalling
+    def get(self, user):
+        '''
+        Get method represents a GET API method
+        '''
+        try:
+            user = str(user)
+            res = FriendsResource.get_pending_friends_request(user)
+            rsp = Response(json.dumps(res), status=200, content_type="application/json")
+        except Exception as e:
+            # HTTP status code.
+            logging.error("/friends/<user>/pending_request, e = {}".format(e))
+            rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+
+        return rsp
+
+#  Restful way of creating APIs through Flask Restful
+class AcceptFriendAPI(MethodResource, Resource):
+    @doc(description='Accept pending friend <friend_id> to be a friend of <user_id>.', tags=['AcceptFriend'])
+    @use_kwargs(FriendRequestSchema, location=('json'))
+    @marshal_with(PostResponseSchema)  # marshalling
+    def post(self, user, **kwargs):
+        '''
+        Post method represents a POST API method
+        '''
+        try:
+            inputs = rest_utils.RESTContext(request)
+            rest_utils.log_request("accept_friend_request", inputs)
+            user = str(user)
+
+            if inputs.method == "POST":
+                friend = inputs.data["friend_id"]
+                res = FriendsResource.accept_friend_request(user, friend)
+                rsp = Response(json.dumps(res), status=200, content_type="application/json")
+            else:
+                rsp = Response("NOT IMPLEMENTED", status=501)
+        except Exception as e:
+            # HTTP status code.
+            logging.error("/friends/<user>/accept, e = {}".format(e))
+            rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+
+        return rsp
+
+api.add_resource(GetFriendAPI, '/friends/<user>')
+api.add_resource(GetPendingFriendAPI, '/friends/<user>/pending')
+api.add_resource(GetRequestFriendAPI, '/friends/<user>/pending_request')
+api.add_resource(AcceptFriendAPI, '/friends/<user>/accept')
+docs.register(GetFriendAPI)
+docs.register(GetPendingFriendAPI)
+docs.register(GetRequestFriendAPI)
+docs.register(AcceptFriendAPI)
+
+# @application.route('/friends/<user>', methods=["GET"])
+# def get_friends(user):
+#     try:
+#         user = str(user)
+#         res = FriendsResource.get_friends(user)
+#         rsp = Response(json.dumps(res), status=200, content_type="application/json")
+#     except Exception as e:
+#         # HTTP status code.
+#         logging.error("/friends/<user>, e = {}".format(e))
+#         rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+#
+#     return rsp
 
 # Return list of pending friends that added you
-@application.route('/friends/<user>/pending', methods=["GET"])
-def get_pending_friends(user):
-    try:
-        user = str(user)
-        res = FriendsResource.get_pending_friends(user)
-        rsp = Response(json.dumps(res), status=200, content_type="application/json")
-    except Exception as e:
-        # HTTP status code.
-        logging.error("/friends/<user>/pending, e = {}".format(e))
-        rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
-
-    return rsp
+# @application.route('/friends/<user>/pending', methods=["GET"])
+# def get_pending_friends(user):
+#     try:
+#         user = str(user)
+#         res = FriendsResource.get_pending_friends(user)
+#         rsp = Response(json.dumps(res), status=200, content_type="application/json")
+#     except Exception as e:
+#         # HTTP status code.
+#         logging.error("/friends/<user>/pending, e = {}".format(e))
+#         rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+#
+#     return rsp
 
 # Return list of pending friends that you added
-@application.route('/friends/<user>/pending_request', methods=["GET"])
-def get_pending_friends_request(user):
-    try:
-        user = str(user)
-        res = FriendsResource.get_pending_friends_request(user)
-        rsp = Response(json.dumps(res), status=200, content_type="application/json")
-    except Exception as e:
-        # HTTP status code.
-        logging.error("/friends/<user>/pending_request, e = {}".format(e))
-        rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+# @application.route('/friends/<user>/pending_request', methods=["GET"])
+# def get_pending_friends_request(user):
+#     try:
+#         user = str(user)
+#         res = FriendsResource.get_pending_friends_request(user)
+#         rsp = Response(json.dumps(res), status=200, content_type="application/json")
+#     except Exception as e:
+#         # HTTP status code.
+#         logging.error("/friends/<user>/pending_request, e = {}".format(e))
+#         rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+#
+#     return rsp
 
-    return rsp
-
-@application.route('/friends/<user>/accept', methods=["POST"])
-def accept_friend_request(user):
-    try:
-        inputs = rest_utils.RESTContext(request)
-        rest_utils.log_request("accept_friend_request", inputs)
-        user = str(user)
-
-        if inputs.method == "POST":
-            friend = inputs.data["friend_id"]
-            res = FriendsResource.accept_friend_request(user, friend)
-            rsp = Response(json.dumps(res), status=200, content_type="application/json")
-        else:
-            rsp = Response("NOT IMPLEMENTED", status=501)
-    except Exception as e:
-        # HTTP status code.
-        logging.error("/friends/<user>/accept, e = {}".format(e))
-        rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
-
-    return rsp
+# @application.route('/friends/<user>/accept', methods=["POST"])
+# def accept_friend_request(user):
+#     try:
+#         inputs = rest_utils.RESTContext(request)
+#         rest_utils.log_request("accept_friend_request", inputs)
+#         user = str(user)
+#
+#         if inputs.method == "POST":
+#             friend = inputs.data["friend_id"]
+#             res = FriendsResource.accept_friend_request(user, friend)
+#             rsp = Response(json.dumps(res), status=200, content_type="application/json")
+#         else:
+#             rsp = Response("NOT IMPLEMENTED", status=501)
+#     except Exception as e:
+#         # HTTP status code.
+#         logging.error("/friends/<user>/accept, e = {}".format(e))
+#         rsp = Response("INTERNAL ERROR", status=500, content_type="text/plain")
+#
+#     return rsp
 
 @application.route('/friends/<user>/decline', methods=["DELETE"])
 def decline_friend_request(user):
